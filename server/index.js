9722aa59ddd2251926941137f7efd0716239fef6
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-09-28 08:22:02 +0800

    set up spaces for routes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ app.use(cors());
 // A full-stack application needs to get data from the client side and the only way to do this is that we have to get it from the `request.body` object.
 app.use(express.json());
 
+/* ROUTES */
+
+// Create a todo
+
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo
+
 app.listen(5000, () => {
   console.log(`server has started on port 5000`);
 });
